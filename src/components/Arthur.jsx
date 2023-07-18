e1dfd2b80af7e231bbae2ee35870e9edaee21b7b
Benfi4rthur <arthur_benfica@hotmail.com>
2023-07-18 16:04:49 -0300

    feat: ajustadas imagens com greyscale no team.

--- src/components/Arthur.jsx
+++ src/components/Arthur.jsx
@@ -1,9 +1,17 @@
+import arthur from '../assets/arthur.jpg';
+
 export default function Arthur() {
-  return (
-    <div className="flex items-center justify-center flex-col bg-slate-700 w-1/3 p-5 rounded-lg">
-      <div>imagem</div>
-      <h1>Arthur</h1>
-      <h1>Desenvolvedor FullStack</h1>
-    </div>
-  );
+    return (
+        <div className='flex items-center justify-center flex-col bg-slate-700 w-1/3 p-5 rounded-lg'>
+            <div>
+                <img
+                    src={arthur}
+                    className='rounded-s-full rounded-r-full grayscale'
+                    style={{ height: '200px', width: '200px' }}
+                />
+            </div>
+            <h1>Arthur</h1>
+            <h1 className='text-lg'>Desenvolvedor FullStack</h1>
+        </div>
+    );
 }
--- src/components/Natacha.jsx
+++ src/components/Natacha.jsx
@@ -1,9 +1,12 @@
-export default function Natasha() {
+import natacha from '../assets/natacha.jpg';
+export default function Natacha() {
     return (
         <div className='flex items-center justify-center flex-col bg-slate-700 w-1/3 p-5 rounded-lg'>
-            <div>imagem</div>
+            <div>
+                <img src={natacha} className='rounded-s-full rounded-r-full grayscale' style={{ height: '200px' }}></img>
+            </div>
             <h1>Natacha</h1>
-            <h1>Designer/Marketing</h1>
+            <h1 className='text-lg'>Designer/Marketing</h1>
         </div>
     );
 }
