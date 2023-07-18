9e5fc3a93942b16876622dcc89d663ba978e9b98
Benfi4rthur <arthur_benfica@hotmail.com>
2023-07-18 18:01:03 -0300

    feat: hover nas imagens dos integrantes.

--- src/components/TeamCard/Card.jsx
+++ src/components/TeamCard/Card.jsx
@@ -1,10 +1,9 @@
 /* eslint-disable react/prop-types */
 export const Card = ({ src = '', Name, Subtitle }) => (
-  <div
-    className='h-72 w-56 flex-shrink-0 rounded-md bg-black/40 py-6 flex gap-4 flex-col items-center'>
+  <div className='h-72 w-56 flex-shrink-0 rounded-md bg-black/40 py-6 flex gap-4 flex-col items-center'>
     <div className='bg-black/30 h-40 w-40 rounded-full overflow-hidden'>
       <img
-        className='object-center object-cover	w-full h-full grayscale'
+        className='object-center object-cover	w-full h-full grayscale hover:transform hover:scale-125 duration-500'
         src={src}
         alt='imagen do integrante da equipe'
       />
