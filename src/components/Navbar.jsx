32e7d410bc947bd4d78d4907ef03164b875a79f3
Benfi4rthur <arthur_benfica@hotmail.com>
2023-07-18 17:48:48 -0300

    feat: inseridos efeitos nos icones e links da navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,21 @@ import logo from '../../public/icone.png';
 export default function Navbar() {
   return (
     <>
-      <div className="flex justify-center ">
-        <header className="flex bg-[#353E69] items-center justify-between p-5 h-[60px] drop-shadow-lg w-2/4 fixed top-2 rounded-2xl ">
-          <a href="/">
-            <img src={logo} className="rounded-xl w-8" alt="Logo" />
-
+      <div className='flex justify-center '>
+        <header className='flex bg-[#353E69] items-center justify-between p-5 h-[60px] drop-shadow-lg w-2/4 fixed top-2 rounded-2xl '>
+          <a href='/'>
+            <img
+              src={logo}
+              className='rounded-xl w-8 hover:transform hover:scale-125 duration-500'
+              alt='Logo'
+            />
           </a>
           <nav>
             <ul className='list-none flex gap-[min(4rem_,_4vw)]'>
-              <li>Inicio</li>
-              <li>Produtos</li>
-              <li>Serviços</li>
-              <li>Contato</li>
+              <li className='cursor-pointer hover:translate-y-1 duration-300'>Inicio</li>
+              <li className='cursor-pointer hover:translate-y-1 duration-300'>Produtos</li>
+              <li className='cursor-pointer hover:translate-y-1 duration-300'>Serviços</li>
+              <li className='cursor-pointer hover:translate-y-1 duration-3300'>Contato</li>
             </ul>
           </nav>
         </header>
