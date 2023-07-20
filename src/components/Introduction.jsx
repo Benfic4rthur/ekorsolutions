2c184618774b2b9fe48c0cfaa989656d750ce1cb
gbprg <duarte.gabrielgbd@gmail.com>
2023-07-20 18:29:16 -0300

    fix: alterando css introduction parte2

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -23,64 +23,65 @@ export function Introduction() {
       className={`font-normal h-[43rem] bg-center bg-cover lg:w-full text-base text-center mt-10 mb-20 rounded-3xl xl:container mx-auto animate-bounce`}
       style={{ backgroundImage: `url(${window.innerWidth > 768 ? banner : iphone})` }}
     >
-      <div className='flex items-center h-full px-3 justify-center'>
-        <div className='w-full sm:w-4/5 max-w-[47.35rem] align-center relative overflow-hidden text-center text-white h-[18rem]'>
+      <div className='flex items-center h-full px-3 justify-center '>
+        <div className='w-full sm:w-4/5 max-w-[47.35rem] items-center relative overflow-hidden text-center text-white h-[18rem]'>
           <Slider {...settings}>
             <div className='flex justify-center items-center flex-col h-full'>
-              <h3 className='text-4xl'>
+              <h3 className='tmobile text-5xl flexCenter'>
                 Suporte Especializado
                 <br />e humanizado
               </h3>
-              <p className='text-base sm:text-2xl'>
+              <p className='text-3xl pmobile'>
                 Um suporte especializado e dedicado para impulsionar o sucesso do seu negócio
               </p>
             </div>
+
             <div className='flex justify-center items-center flex-col h-full'>
-              <h3 className='text-4xl sm:text-6xl'>
+              <h3 className='tmobile text-5xl flexCenter'>
                 Site personalizado
                 <br />
                 conforme necessidade
               </h3>
-              <p className='text-base sm:text-2xl'>
+              <p className='text-3xl pmobile'>
                 Transformamos suas ideias em realidade, <br />
                 criando sites e aplicações web personalizadas <br />
                 que refletem a essência do seu negócio
               </p>
             </div>
             <div className='flex justify-center items-center flex-col h-full'>
-              <h3 className='text-4xl sm:text-6xl'>
+              <h3 className='tmobile text-5xl flexCenter'>
                 Design criativo
                 <br />e funcional
               </h3>
-              <p className='text-base sm:text-2xl'>
+              <p className='text-3xl pmobile'>
                 Projetos de design criativos e inovadores
                 <br />
                 criando experiencias unicas para nossos clientes
               </p>
             </div>
             <div className='flex justify-center items-center flex-col'>
-              <h2 className='text-5xl sm:text-6xl'>Assinatura digital</h2>
-              <p className='text-lg sm:text-2xl'>
+              <h3 className='tmobile text-5xl flexCenter'>Assinatura digital</h3>
+              <p className='tmobile text-5xl flexCenter'>
                 Segurança e confiabilidade para sua empresa em transações online
               </p>
             </div>
             <div className='flex justify-center items-center flex-col h-full'>
-              <h3 className='text-4xl sm:text-6xl'>
+              <h3 className='tmobile text-5xl flexCenter'>
                 Engenharia voltada
                 <br />
                 para o futuro
               </h3>
-              <p className='text-base sm:text-2xl'>
+              <p className='text-3xl pmobile'>
                 Projetos de engenharia precisos e baseados em modelagem matemática, garantindo
                 resultados confiáveis e eficientes
               </p>
             </div>
             <div className='flex justify-center items-center flex-col h-full'>
-              <h3 className='text-4xl sm:text-6xl'>
+              <h3 className='tmobile text-5xl flexCenter'>
                 Consultoria Especializada
                 <br />
               </h3>
-              <p className='text-base sm:text-2xl'>
+              <p className='text-3xl pmobile'>
                 Consultoria voltada as plataformas .jus e a novos desenvolvedores com dificuldade em
                 areas sensíveis
               </p>
