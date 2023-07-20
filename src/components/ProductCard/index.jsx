2d8debdd8fc49461eb6bd5f3befe3e0a0203fede
Benfi4rthur <arthur_benfica@hotmail.com>
2023-07-20 12:25:07 -0300

    feat: ajustado cardright

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -38,9 +38,9 @@ export const ProductCardLeft = ({ title, description, src }) => (
 export const ProductCardRight = ({ title, description, src }) => (
   <div className=' w-full grid grid-rows-[auto, auto] md:grid-cols-2 items-center gap-3 md:gap-[3%] p-2'>
     <div className='aspect-video bg-black/30 rounded-xl overflow-hidden animate-showingReverse'>
-      <img src={src} className='object-center object-cover	min-w-full min-h-full' />
+      <img src={src} className='object-center object-cover	min-w-full' />
     </div>
-    <div className='h-full flex-col gap-3 flex justify-center animate-showing md:-order-1'>
+    <div className='flex-col gap-3 flex justify-center animate-showing md:-order-1'>
       <h3 className='text-3xl md:text-4xl text-slate-50'>{title}</h3>
       <p className='font-medium text-base lg:text-lg  text-slate-50'>{description}</p>
     </div>
