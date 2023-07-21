d6333d8633ea25ebce29c75b59d6dfca24b56290
Benfi4rthur <arthur_benfica@hotmail.com>
2023-07-21 15:22:23 -0300

    feat: ajustado nome do mediaverse.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import vikore from '../assets/vikore.mp4';
+import mediaverse from '../assets/mediaverse.mp4';
 import epet from '../assets/epet.jpg';
 import { ProductCardLeft, ProductCardRight } from './ProductCard';
 
@@ -10,12 +10,12 @@ export function Products() {
         <div className='flex flex-col gap-6'>
           <ProductCardLeft
             description='Potencialize seu negócio com nossa plataforma de streaming especializada em treinamentos corporativos, cursos online e aluguel de vídeos sob demanda. Compartilhe conteúdo eficiente de forma envolvente para colaboradores, clientes e estudantes. Descubra recursos personalizáveis e intuitivos que revolucionarão sua maneira de compartilhar conhecimento.'
-            title='VIKORE - Vídeos Corporativos para Educação'
-            src={vikore}
+            title='MediaVerse - Treinamentos Empresariais Multimídia'
+            src={mediaverse}
           />
           <ProductCardRight
             description='Conheça o nosso aplicativo de petshop! Agende serviços, venda produtos e acompanhe as novidades da sua loja favorita. Gerencie seu petshop de forma eficiente, oferecendo conveniência aos clientes e aumentando as vendas. Simplifique a vida dos amantes de animais. Acesse agora e aproveite!'
-            title='E-PET'
+            title='E-Pet'
             src={epet}
           />
         </div>
